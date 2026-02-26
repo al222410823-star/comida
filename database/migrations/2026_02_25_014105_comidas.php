63ecdff541ddc942c1726a86384b4e14f9c538c9
al222410823-star <al222410823@gmail.com>
2026-02-26 12:16:50 -0600

    Add comments and improve migration clarity

--- database/migrations/2026_02_25_014105_comidas.php
+++ database/migrations/2026_02_25_014105_comidas.php
@@ -8,21 +8,21 @@ return new class extends Migration
 {
     public function up(): void
     {
-         
+        // TABLA 1: Categorías 
         Schema::create('tb_tipo_comidas', function (Blueprint $table) {
-            $table->id('id_tipo_comida'); 
+            $table->id('id_tipo_comida'); // Llave Primaria
             $table->enum('nombre_categoria', ['Bebidas', 'Postres', 'Platillos Fuertes', 'Entradas', 'Sopas']);
-            $table->timestamps(); 
+            $table->timestamps(); // created_at y updated_at
         });
 
-        
+        // TABLA 2: Comidas (La que tiene la llave foránea)
         Schema::create('tb_comidas', function (Blueprint $table) {
-            $table->id('id_comida'); 
+            $table->id('id_comida'); // Llave Primaria
             $table->string('nombre_comida', 100);
             $table->decimal('costo', 8, 2);
             $table->text('detalle_comida');
             
-            
+            // Relación con la tabla de arriba
             $table->unsignedBigInteger('id_tipo_comida'); 
             $table->foreign('id_tipo_comida')
                   ->references('id_tipo_comida')
